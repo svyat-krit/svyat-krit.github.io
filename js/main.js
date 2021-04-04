cdbb41b3380a6fd193f8b945d589f45eab85fb12
Svyat Krit <krit.mgmn@gmail.com>
2021-04-04 16:43:04 +0300

    Updated render options

--- js/main.js
+++ js/main.js
@@ -19,25 +19,22 @@ function init() {
   light.position.set( 50, 50, 50 );
   scene.add( light );
 
-  const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
   const material = new THREE.MeshLambertMaterial({color: 0xffffff, ambient: 0x121212, emissive:0x121212});
 
   const cubeArray = [];
 
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 500; i++) {
     const cube = new THREE.Mesh( geometry, material );
 
-    cube.position.x = Math.random() * 300 * (Math.random() > 0.5 ? 1 : -1);
-    cube.position.y = Math.random() * 300 * (Math.random() > 0.5 ? 1 : -1);
-    cube.position.z = Math.random() * 300 * (Math.random() > 0.5 ? 1 : -1);
+    cube.position.x = Math.random() * 200 * (Math.random() > 0.5 ? 1 : -1);
+    cube.position.y = Math.random() * 200 * (Math.random() > 0.5 ? 1 : -1);
+    cube.position.z = Math.random() * 200 * -1;
     
     cubeArray.push(cube); 
 
     scene.add( cube );
   }
-
-  camera.position.y = 5;
-  camera.position.z = 15;
 
   function render() {
     requestAnimationFrame( render );
@@ -52,8 +49,8 @@ function init() {
   });
   
   window.addEventListener("mousemove", (event) => {
-    scene.position.x = (event.clientX - window.innerWidth / 2) / 20;
-    scene.position.y = (event.clientY - window.innerHeight / 2) / 20;
+    scene.position.x = (event.clientX - window.innerWidth / 2) / 50;
+    scene.position.y = (event.clientY - window.innerHeight / 2) / 50;
   });
 }
 
