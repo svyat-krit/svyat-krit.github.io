75ccc328e42b53e57db0db272b6dc4695d9778c8
Svyat Krit <krit.mgmn@gmail.com>
2021-04-04 12:14:33 +0300

    Added controls to 3d object

--- js/main.js
+++ js/main.js
@@ -1,51 +1,54 @@
 'use strict';
 
-let scene = new THREE.Scene();
+init();
 
-let camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+function init() {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  const renderer = new THREE.WebGLRenderer( { antialias: true } );
 
-let renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
 
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+  const light = new THREE.HemisphereLight( 0xffffff, 0x080820, 1 );
+  light.position.set( 50, 50, 50 );
+  scene.add( light );
 
-const light = new THREE.HemisphereLight( 0xffffff, 0x080820, 1 );
-light.position.set( 50, 50, 50 );
-scene.add( light );
+  const geometry = new THREE.BoxGeometry( 5, 5, 5 );
+  const material = new THREE.MeshLambertMaterial({color: 0xffffff, ambient: 0x121212, emissive:0x121212});
+  const cube = new THREE.Mesh( geometry, material );
 
-const geometry = new THREE.BoxGeometry( 5, 5, 5 );
-const material = new THREE.MeshLambertMaterial({color: 0xffffff, ambient: 0x121212, emissive:0x121212});
-const cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
 
-scene.add( cube );
+  camera.position.y = 5;
+  camera.position.z = 15;
 
-camera.position.z = 25;
+  const controls = new THREE.OrbitControls( camera, renderer.domElement );
 
-function render() {
-  requestAnimationFrame( render );
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.005;
-  renderer.render( scene, camera );
+  function render() {
+    requestAnimationFrame( render );
+    cube.rotation.x += 0.013;
+    cube.rotation.y += 0.005;
+    controls.update();
+    renderer.render( scene, camera );
+  }
+
+  render();
+
+  window.addEventListener("resize", () => {
+    onWindowResize(camera, renderer, scene);
+  });
 }
 
-render();
-
-function onWindowResize() {
-
+function onWindowResize(camera, renderer, scene) {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
   renderer.setSize( window.innerWidth, window.innerHeight );
-
   renderer.render( scene, camera );
-
 }
-
-window.addEventListener("resize", () => {
-  onWindowResize();
-});
